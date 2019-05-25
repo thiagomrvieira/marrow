f103d85a92e3056d9c0b5f1e8cf73594d2ef8fbb
Thiago Maciel <thiagomrvieira@gmail.com>
2019-05-25 13:56:55 -0300

    Ajuste no migration agendamentos

--- database/migrations/2019_04_15_125733_create_agendamentos_table.php
+++ database/migrations/2019_04_15_125733_create_agendamentos_table.php
@@ -15,12 +15,10 @@ class CreateAgendamentosTable extends Migration
     {
         Schema::create('agendamentos', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->nullable();
+            $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->integer('posto_id')->nullable();
+            $table->integer('posto_id')->unsigned();
             $table->foreign('posto_id')->references('id')->on('postos');
-            $table->integer('doacao_id')->nullable();
-            $table->foreign('doacao_id')->references('id')->on('doacoes');
             $table->string('tipo');
             $table->dateTime('data');
             $table->longText('observacoes')->nullable();
